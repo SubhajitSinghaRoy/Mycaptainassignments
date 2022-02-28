1856a8471cd7ac1f752c313eb2dee2909f0d64e5
Subhajit Singha Roy <98935490+SubhajitSinghaRoy@users.noreply.github.com>
2022-02-28 11:24:13 +0530

    Update using functions.py

--- using functions.py
+++ using functions.py
@@ -1,9 +1,12 @@
 #python program to find how many times the letters occur in the word
-random_list = input("enter the word to find the frequency of the letters:\n")
-frequency = {}
-for item in random_list:
-   if item in frequency:
-    frequency[item] += 1
-   else:
-    frequency[item] = 1
-print(frequency)
+def most_frequent():
+    str1 = str(input("Enter the word to find the frequency of the word:\n"))
+    d = dict()
+    for i in str1:
+        d[i] = str1.count(i)
+    for i in range(len(d)):
+        max_1 = max(d, key=d.get)
+        max_2 = max(d.values())
+        print("{} = {}".format(max_1, max_2))
+        d.pop(max_1)
+most_frequent()
